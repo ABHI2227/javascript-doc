6435e252553727ef788f98fec423fa55fe42e6a6
Abhishek <malveabhishek2000@gmail.com>
2024-08-02 13:36:56 +0530

    Conversion Operation in js

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 console.log("Abhishek");
 
 // number => 2 to power 53
@@ -7,3 +8,16 @@ console.log("Abhishek");
 // char =>
 // null => standalone value 
 // undefined =>
+=======
+console.log("Abhishek");
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// char =>
+// null => standalone value 
+// undefined =>
+
+    console.log(typeof 1 );
+>>>>>>> Stashed changes
