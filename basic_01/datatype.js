8dde5299b79f11992b729144f9a358112113c61c
Abhishek <malveabhishek2000@gmail.com>
2024-08-10 15:48:51 +0530

    deatileld datatype

---
+++ basic_01/datatype.js
@@ -0,0 +1,41 @@
+//learning is easy just be aware of the flow and you good to go...
+
+// javaScript dosnot have any relation with the JAVA its just at the 
+// time js was invented that time java was popular and there makers thought it
+//  will be grate marketing move if we named it as javaScript to make it popular as java
+
+//JS elimenates spaces in between the code 
+//It was dynamically typed language so veriable declaration doest need to wrote type 
+
+//java script is most popular programming language 
+//easy to learn to those who want to learn
+
+//it also called as browser's lang cause it run's over the browser but after "node" it is mainly runs everyewhere from frontend to backend 
+
+
+// js have 7 datatype: 
+
+let intValue = 1;                                console.log(typeof intValue);
+let stringValue = "Abhishek";                    console.log(typeof stringValue);
+let BigIntValue = 789465132568974651256623n;     console.log(typeof BigIntValue);
+let BooleanValue = true;                         console.log(typeof BooleanValue);  //boolean represent's ; 0 = false; 1 = true;
+let charValue = "a";                             console.log(typeof charValue);
+let nullValue = null;                            console.log(typeof nullValue);
+let sybolValue = undefined;                      console.log(typeof aValue);
+
+// javascript is case sensetive origramming language it means:
+
+let Abhi = 123;
+let abhi = 13; //both are different from each other lets print them:
+
+console.log(Abhi);
+console.log(abhi);
+
+// JS is mainly using the CameCasing to write the code 
+
+// Prefix (++variable or --variable): The variable is modified (incremented or decremented) before being used in the expression.
+// Postfix (variable++ or variable--): The variable is used in the expression first, and then it is modified (incremented or decremented).
+// Use Cases:
+// Prefix: Useful when you want to immediately use the updated value.
+// Postfix: Useful when you need to use the current value before it changes.
+
