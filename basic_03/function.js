dd45c9c9bdddfdb5dec7126775c989a9f793fa97
Abhishek <malveabhishek2000@gmail.com>
2024-08-13 13:07:43 +0530

    function in JS

--- basic_03/function.js
+++ basic_03/function.js
@@ -1,5 +1,4 @@
 //function in JS
-
 // console.log("A");
 // console.log("B");
 // console.log("H");
@@ -21,9 +20,32 @@ console.log("S");
 console.log("H");
 console.log("E");
 console.log("K");
+
 }
 
 
-sayMyName() //sayMyname is referance
+// sayMyName() //sayMyname is referance
 
- 
+
+
+ function add(a,b ){
+
+    // let result = a  + b
+    // console.log("abhishek")
+    return a + b
+
+ }
+
+const result = add(1,3)
+// console.log( "Result: " , result) //
+
+
+function loginUser(username = "sam"){
+    if(!username){
+        console.log("please Enter a User Name");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUser("abhishek"))
